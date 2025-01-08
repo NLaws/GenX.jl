8ff20d2de9167d2ac15f85991ed07ed67ce4603f
Nick Laws <laws.nick@gmail.com>
2025-01-07 20:55:14 -0700

    split market results into two CSVs, 1 for time series, 1 for summary

--- src/write_outputs/write_market_results.jl
+++ src/write_outputs/write_market_results.jl
@@ -9,15 +9,24 @@ function write_market_results(path::AbstractString, inputs::Dict, setup::Dict, E
 
     mwh_purchases = Dict()
     for m=1:M
-        mwh_purchases["total_purchases_mwh_tier_$m"] = sum(
-            JuMP.value.(EP[:vMarketPurchaseMW][:, :, m])
-        ) * scale_factor
+        mwh_purchases["hourly_purchases_mwh_tier_$m"] = 
+            vec(JuMP.value.(EP[:vMarketPurchaseMW][:, :, m])) * scale_factor
     end
-    mwh_purchases["total_purchases_mwh"] = sum(values(mwh_purchases))
 
     df = DataFrame(mwh_purchases)
-    df[!, "total_mwh_sales"] = [sum(JuMP.value.(EP[:vMarketSaleMW][:, SELL_TIER]))] * scale_factor
-    df[!, "total_sales_benefit"] = [JuMP.value(EP[:eMarketSalesBenefit])] * scale_factor^2
-    df[!, "total_purchases_cost"] = [JuMP.value(EP[:eMarketPurchasesCost])] * scale_factor^2
+    hourly_sales = vec(JuMP.value.(EP[:vMarketSaleMW][:, SELL_TIER])) * scale_factor
+
+    df[!, "hourly_sales_mwh"] = hourly_sales
+
+    CSV.write(joinpath(path, "market_results_time_series.csv"), df)
+
+    summary = Dict()
+    summary["total_mwh_sales"] = sum(sum(hourly_sales))
+    summary["total_sales_benefit"] = [JuMP.value(EP[:eMarketSalesBenefit])] * scale_factor^2
+    summary["total_mwh_purchases"]= sum(sum(values(mwh_purchases)))
+    summary["total_purchases_cost"] = [JuMP.value(EP[:eMarketPurchasesCost])] * scale_factor^2
+    df = DataFrame(summary)
+
     CSV.write(joinpath(path, "market_results.csv"), df)
+
 end
